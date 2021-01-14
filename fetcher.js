c23919030c4aba2bd1444d23b944014f5da076d1
dantan123 <dtan9988@gmail.com>
2021-01-14 06:07:52 +0000

    implement file downloader

---
+++ fetcher.js
@@ -0,0 +1,36 @@
+const request = require('request');
+const fs = require('fs');
+
+const requestContent = function(input) {
+  if (input.length < 4) {
+    console.log('Not enough arguments');
+    return;
+  }
+  const inputArrays = input.slice(2);
+  let URL = inputArrays[0];
+  let localFile = inputArrays[1];
+
+  request(URL, (error, response, body) => {
+    if (error) {
+      console.log(error, response, response.statusCode);
+    }
+
+    fs.writeFile(localFile, body, (error) => {
+      if (error) { 
+        console.log(error);
+      } else {
+        fs.stat(localFile, (err, stats) => {
+          console.log(`Downloaded and saved ${stats.size} bytes to ${localFile}`);
+        })
+      }
+    })
+  })
+};
+
+requestContent(process.argv);
+
+// request('https://www.google.com/fdsafsafsa.html', (error, response, body) => {
+//   console.log('error: ', error);
+//   console.log('response: ', response && response.statusCode);
+//   console.log('body: ', body);
+// });
